d7918ac2bf067c3c8b660a577e6814eae59afc4a
Gaurav Trivedi <50577208+gautriv@users.noreply.github.com>
2024-10-02 16:27:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+<p>Contact Us:</p>
+<ul>
+    <li>
+        <a href="mailto:support@example.com?subject=Customer%20Inquiry">Email Us via Your Email Client</a>
+    </li>
+    <li>
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=support@example.com&su=Customer%20Inquiry" target="_blank">Email Us via Gmail</a>
+    </li>
+    <li>
+        <a href="https://outlook.live.com/owa/?path=/mail/action/compose&to=support@example.com&subject=Customer%20Inquiry" target="_blank">Email Us via Outlook.com</a>
+    </li>
+    <li>
+        <a href="https://compose.mail.yahoo.com/?to=support@example.com&subject=Customer%20Inquiry" target="_blank">Email Us via Yahoo Mail</a>
+    </li>
+</ul>
